68e6f10d8c463838300682c0adc333d2a3c45cd4
ppr350 <kobito610@gmail.com>
2024-06-02 16:22:35 +0100

    Set Up NPM and Webpack successfully

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import './style.css';
+
+// function mergeSort() {
+//     let 
+// }
+
+// function merge() {
+//     let sortArr = []
+
+// }
+
+console.log('Project Binary Search Trees')
