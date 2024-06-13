7b7ab9273a1973435c533d6cd1dc1044c729259b
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-06-13 12:49:20 +0100

    Added TOP instructions for what to do. Created a new file called driver.js that ties the whole project together

---
+++ src/driver.js
@@ -0,0 +1,9 @@
+// Write a driver script that does the following:
+// 1. Create a binary search tree from an array of random numbers < 100. You can can create a function that returns an array of random numbers every time you call it if you wish
+// 2. Confirms that the tree is balanced by calling isBalanced
+// 3. Print out all elements in level, pre, post, and in order
+// 4. Unbalance the tree by adding several number > 100
+// 5. Confirm that the tree is unbalanced by calling isBalanced
+// 6. Balance the tree by calling rebalance
+// 7. Confirm that the tree is balanced by calling isBalanced
+// 8. Print out all elements in level, pre, post, and in order
--- src/tree.js
+++ src/tree.js
@@ -10,6 +10,7 @@ class Tree {
         this.print = this.prettyPrint(this.root)
     }
 
+    // Write a buildTree(array) function that takes an array of data and turns it into a balanced binary tree full of Node objects appropriately placed
     buildTree(array, start, end) {
         console.log(array, start, end)
         if (start > end) {
@@ -22,6 +23,71 @@ class Tree {
         return node
     }
 
+    // Write an insert(data) that insert the given data
+    insert(data) {
+
+    }
+    
+    // Write a deleteItem(data) that delete the given data
+    deleteItem(data) {
+
+    }
+
+    // Write a find(data) function that returns the node with the given data
+    find(data) {
+
+    }
+
+    // Write a levelOrder(callback) that accepts an optional callback function as its parameter
+    // levelOrder should traverse the tree in breadth-first level order and provide each node as an argument to the callback
+    // As a result, the callback will perform an operation on each node following the order in which they're traversed
+    // levelOrder may be implemented using either iteration or recursion
+    // The method should return an array of values if no callback is given as an argument
+    // Tip: You will want to to use an array acting as a queue to keep track of all the child nodes that you have yet to traverse and to add new ones to the list
+    levelOrder(callback) {
+        
+    }
+
+    // Write inOrder(callback), preOrder(callback), and postOrder(callback) functions that also accept an optional callback as a parameter
+    // Each of these should traverse the tree in their respective depth-first order and yield each node to the provided callback
+    // The functions should return an array of values if no callback is given as an argument
+    inOrder(callback) {
+
+    }
+
+    preOrder(callback) {
+
+    }
+
+    postOrder(callback) {
+
+    }
+
+    // Write a height(node) function that returns the given node's height
+    // Height is defined as the number of edges in the longest path from a given node to a leaf node
+    height(node) {
+
+    }
+
+    // Write a depth(node) function that returns the given node's depth
+    // Depth is defines as the number of edges in the path from a given node to the tree's root node
+    depth(node) {
+
+    }
+
+    // Write an isBalanced function that checks if the tree is balanced
+    // A balanced tree is one where the difference between heights of the left subtree and the right subtree of every node is not more than 1
+    isBalanced() {
+
+    }
+
+    // Write a rebalance function that rebalances an unbalanced tree
+    // Tip: You'll want to use a traversal method to provide a new array to the buildTree function
+    rebalance() {
+
+    }
+
+    // Visualize your binary tree
     prettyPrint(node, prefix = '', isLeft = true) {
         if (node === null) {
             return
